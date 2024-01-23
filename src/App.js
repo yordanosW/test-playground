b9fe4f559451a6b8e72c41fc95c7c86bd320b294
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2024-01-23 14:16:04 -0800

    complit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [btnColor, setBtnColor] = useState('green')
+  const newBtnColor = btnColor === 'green'? 'blue' : 'green'
+  const [inputDisabled, setInputDisabled] = useState(false)
+  const [buttonStatus, setButtonStatus] = useState("Button is disabled")
+  const newButtonStatus = buttonStatus ==="Button is enabled" ? "Button is disabled" : "Button is enable"
+
+ return (
+    <div clsssName="App">
+
+     <h1>Testing Playground</h1>
+
+     <button 
+     style={{backgroundColor: btnColor}} 
+     onClick={() => setBtnColor(newBtnColor)}
+     disabled={inputDisabled}
+     >
+      Change  to {newBtnColor}
+     </button>
+
+     <input
+     type="checkbox"
+     defaultChecked={inputDisabled}
+     onChange={(event)=>
+     setInputDisabled(event.target.checked)}
+     onClick={()=> setButtonStatus(newButtonStatus)}
+     />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import App from './App'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('test for header', () => {
+  test('header renders with correct text', () => {
+    render(<App />)
+    const headerEl = screen.getByRole("heading")
+    expect(headerEl.textContent).toBe("Testing Playground")
+  })
+})
+
+describe('test for the button', () =>{
+  test("button changes color when clicked", () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+  
+    userEvent.click(colorBtn)
+  
+    expect(colorBtn).toHaveStyle({ backgroundColor: "blue" })
+    expect(colorBtn.textContent).toBe("Change button color to green")
+  })
+})
+
+describe('tests related to checkbox and enabling/disabling button', () => {
+  test("checkbox disables button when first clicked, then enables button on second click", () => {
+    render(<App />)
+    const colorBtn = screen.getByRole("button")
+    const checkbox = screen.getByRole("checkbox")
+  
+    userEvent.click(checkbox)
+    expect(colorBtn).toBeDisabled()
+  
+    userEvent.click(checkbox)
+    expect(colorBtn).toBeEnabled()
+  })
+
+  test("accurate text appears when button is enabled or disabled", () => {
+    render(<App />)
+    const checkbox = screen.getByRole("checkbox")
+    const paragraphEl = screen.getByRole("paragraph")
+  
+    expect(paragraphEl.textContent).toBe("Button is enabled")
+  
+    userEvent.click(checkbox)
+    expect(paragraphEl.textContent).toBe("Button is disabled")
+  
+    userEvent.click(checkbox)
+    expect(paragraphEl.textContent).toBe("Button is enabled")
+  })
+ 
+})
+
+
